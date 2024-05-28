892a611081b23368d334293ff6115b4ba879fa97
DarkDeuse <sasha12345678_92@mail.ru>
2024-05-29 01:56:20 +0300

    Перезалив работы, только теперь в отдельный репозиторий, после восстановения компьютера

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "ip_filter.hpp"
+
+#include <fstream>
+
+
+int main(int, char const* []) {
+	try {
+
+		auto ip_pool{ input_ip() };
+
+		auto vect_string_to_int{ vector_string_to_int(ip_pool) };
+
+		reverse_lexicographically_sort(vect_string_to_int);
+
+		print_ip(vect_string_to_int);
+
+		print_ip(filter(vect_string_to_int, 1));
+
+		print_ip(filter(vect_string_to_int, 46, 70));
+
+		print_ip(filter_any(vect_string_to_int, 46));
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
